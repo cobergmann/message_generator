1403f8f933b5d4f45225130859439818c5653f94
Conrad Bergmann <conradbergmann1@gmail.com>
2024-12-22 17:34:01 +0100

    First version of message generator

---
+++ main.js
@@ -0,0 +1,12 @@
+// Randomly return elements from a given array
+const randomReturn = array => {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// Define parts of the sentences
+spacePart = ['The weather', 'The time', 'The group', 'The house']
+timePart = ['was', 'is', 'will be']
+mood = ['good', 'bad']
+
+// Generate messages
+console.log(`${randomReturn(spacePart)} ${randomReturn(timePart)} ${randomReturn(mood)}`)
